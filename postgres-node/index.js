4f41d38900cc645dcd64f49ea97ee68caab30b41
Forstrei <ajdraper96@gmail.com>
2023-06-09 21:01:57 -0600

    Switched connection type from client to pool

--- postgres-node/index.js
+++ postgres-node/index.js
@@ -1,51 +1,53 @@
 // code generously supplied by:
 // https://www.makeuseof.com/node-postgresql-connect-how/
-const { Client } = require("pg");
-const dotenv = require("dotenv");
-dotenv.config();
 
-const connectDb = async () => {
-    try {
-        const client = new Client({
-            user: process.env.PGUSER,
-            host: process.env.PGHOST,
-            database: process.env.PGDATABASE,
-            password: process.env.PGPASSWORD,
-            port: process.env.PGPORT
-        });
 
-        await client.connect();
-        const res = await client.query('SELECT * FROM pokemon');
-        console.log(res);
-        await client.end();
-    } catch (error) {
-        console.log(error);
-    }
-};
-
-connectDb();
-
-// const { Pool } = require("pg");
+// const { Client } = require("pg");
 // const dotenv = require("dotenv");
 // dotenv.config();
 
 // const connectDb = async () => {
 //     try {
-//         const pool = new Pool({
+//         const client = new Client({
 //             user: process.env.PGUSER,
 //             host: process.env.PGHOST,
 //             database: process.env.PGDATABASE,
 //             password: process.env.PGPASSWORD,
-//             port: process.env.PGPORT,
+//             port: process.env.PGPORT
 //         });
 
-//         await pool.connect();
-//         const res = await pool.query('SELECT * FROM clients');
+//         await client.connect();
+//         const res = await client.query('SELECT * FROM pokemon');
 //         console.log(res);
-//         await pool.end();
+//         await client.end();
 //     } catch (error) {
 //         console.log(error);
 //     }
 // };
 
 // connectDb();
+
+const { Pool } = require("pg");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const connectDb = async () => {
+    try {
+        const pool = new Pool({
+            user: process.env.PGUSER,
+            host: process.env.PGHOST,
+            database: process.env.PGDATABASE,
+            password: process.env.PGPASSWORD,
+            port: process.env.PGPORT,
+        });
+
+        await pool.connect();
+        const res = await pool.query('SELECT * FROM pokemon');
+        console.log(res);
+        await pool.end();
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+connectDb();
