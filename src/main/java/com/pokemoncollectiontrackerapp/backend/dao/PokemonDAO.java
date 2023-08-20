0c27549341c1b571f180c9abe757a3bd81737f04
Forstrei <ajdraper96@gmail.com>
2023-08-19 23:02:09 -0600

    Commented setters for pokemon model

--- src/main/java/com/pokemoncollectiontrackerapp/backend/dao/PokemonDAO.java
+++ src/main/java/com/pokemoncollectiontrackerapp/backend/dao/PokemonDAO.java
@@ -20,21 +20,21 @@ public class PokemonDAO {
 	public List<PokemonModel> jank(){
 		String sql = "SELECT * FROM pokemon";
 		return jdbcTemplateImplementation.query(sql, (rs, rowNum) -> {
-			PokemonModel pokemon = new PokemonModel();
-			pokemon.set_id(rs.getInt("pokemon_id"));
-			pokemon.setNational_dex_number(rs.getInt("national_dex_number"));
-			pokemon.setName(rs.getString("name"));
-			pokemon.setPrimary_type(rs.getString("primary_type"));
-			pokemon.setSecondary_type(rs.getString("secondary_type"));
-			pokemon.setCategory(rs.getString("category"));
-			pokemon.setPrimary_ability(rs.getString("primary_ability"));
-			pokemon.setSecondary_ability(rs.getString("secondary_ability"));
-			pokemon.setHidden_ability(rs.getString("hidden_ability"));
-			pokemon.setMega_ability(rs.getString("mega_ability"));
-			pokemon.setGender_ratio(rs.getString("gender_ratio"));
-			pokemon.setCatch_rate(rs.getInt("catch_rate")); 
-			pokemon.setE_G_G_1(rs.getString("primary_egg_group"));
-			pokemon.setE_G_G_2(rs.getString("secondary_egg_group"));
+			PokemonModel pokemon = new PokemonModel(
+			rs.getInt("national_dex_number"),
+			rs.getInt("pokemon_id"),
+			rs.getString("name"),
+			rs.getString("primary_type"),
+			rs.getString("secondary_type"),
+			rs.getString("category"),
+			rs.getString("primary_ability"),
+			rs.getString("secondary_ability"),
+			rs.getString("hidden_ability"),
+			rs.getString("mega_ability"),
+			rs.getString("gender_ratio"),
+			rs.getInt("catch_rate"),
+			rs.getString("primary_egg_group"),
+			rs.getString("secondary_egg_group"));
 			return pokemon;
 		});
 		// List<Map<String, Object>> myList = this.jdbcTemplateImplementation.queryForList(sql);
--- src/main/java/com/pokemoncollectiontrackerapp/backend/model/PokemonModel.java
+++ src/main/java/com/pokemoncollectiontrackerapp/backend/model/PokemonModel.java
@@ -52,87 +52,87 @@ public class PokemonModel {
     public int get_id() {
         return pokemon_id;
     }
-    public void set_id(int pokemon_id) {
-        this.pokemon_id = pokemon_id;
-    }
+    // public void set_id(int pokemon_id) {
+    //     this.pokemon_id = pokemon_id;
+    // }
     public int getNational_dex_number() {
         return national_dex_number;
     }
-    public void setNational_dex_number(int national_dex_number) {
-        this.national_dex_number = national_dex_number;
-    }
+    // public void setNational_dex_number(int national_dex_number) {
+    //     this.national_dex_number = national_dex_number;
+    // }
     public String getName() {
         return name;
     }
-    public void setName(String name) {
-        this.name = name;
-    }
+    // public void setName(String name) {
+    //     this.name = name;
+    // }
     public String getPrimary_type() {
         return primary_type;
     }
-    public void setPrimary_type(String primary_type) {
-        this.primary_type = primary_type;
-    }
+    // public void setPrimary_type(String primary_type) {
+    //     this.primary_type = primary_type;
+    // }
     public String getSecondary_type() {
         return secondary_type;
     }
-    public void setSecondary_type(String secondary_type) {
-        this.secondary_type = secondary_type;
-    }
+    // public void setSecondary_type(String secondary_type) {
+    //     this.secondary_type = secondary_type;
+    // }
     public String getCategory() {
         return category;
     }
-    public void setCategory(String category) {
-        this.category = category;
-    }
+    // public void setCategory(String category) {
+    //     this.category = category;
+    // }
     public String getPrimary_ability() {
         return primary_ability;
     }
-    public void setPrimary_ability(String primary_ability) {
-        this.primary_ability = primary_ability;
-    }
+    // public void setPrimary_ability(String primary_ability) {
+    //     this.primary_ability = primary_ability;
+    // }
     public String getSecondary_ability() {
         return secondary_ability;
     }
-    public void setSecondary_ability(String secondary_ability) {
-        this.secondary_ability = secondary_ability;
-    }
+    // public void setSecondary_ability(String secondary_ability) {
+    //     this.secondary_ability = secondary_ability;
+    // }
     public String getHidden_ability() {
         return hidden_ability;
     }
-    public void setHidden_ability(String hidden_ability) {
-        this.hidden_ability = hidden_ability;
-    }
+    // public void setHidden_ability(String hidden_ability) {
+    //     this.hidden_ability = hidden_ability;
+    // }
     public String getMega_ability() {
         return mega_ability;
     }
-    public void setMega_ability(String mega_ability) {
-        this.mega_ability = mega_ability;
-    }
+    // public void setMega_ability(String mega_ability) {
+    //     this.mega_ability = mega_ability;
+    // }
     public String getGender_ratio() {
         return gender_ratio;
     }
-    public void setGender_ratio(String gender_ratio) {
-        this.gender_ratio = gender_ratio;
-    }
+    // public void setGender_ratio(String gender_ratio) {
+    //     this.gender_ratio = gender_ratio;
+    // }
     public int getCatch_rate() {
         return catch_rate;
     } 
-    public void setCatch_rate(int catch_rate) {
-        this.catch_rate = catch_rate;
-    } 
+    // public void setCatch_rate(int catch_rate) {
+    //     this.catch_rate = catch_rate;
+    // } 
     public String getE_G_G_1() {
         return E_G_G_1;
     }
-    public void setE_G_G_1(String E_G_G_1) {
-        this.E_G_G_1 = E_G_G_1;
-    }
+    // public void setE_G_G_1(String E_G_G_1) {
+    //     this.E_G_G_1 = E_G_G_1;
+    // }
     public String getE_G_G_2() {
         return E_G_G_2;
     }
-    public void setE_G_G_2(String E_G_G_2) {
-        this.E_G_G_2 = E_G_G_2;
-    }
+    // public void setE_G_G_2(String E_G_G_2) {
+    //     this.E_G_G_2 = E_G_G_2;
+    // }
 
 };
 
